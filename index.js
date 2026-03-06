7c1fe74b82153e1fc759c4ed49c025fad7c5dff4
Aengus MacKenzie <aengus.2000@gmail.com>
2026-03-06 02:53:45 -0500

    saving to restart pc

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 //Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+};
+
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+};
+
+function updateTicketPrice(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+};
+
+function updateTicketPrice (attendee, newTicketPrice) {
+  attendee.ticketPrice = newTicketPrice;
+};
 
 
 //Needed for the tests to work. Don't modify
